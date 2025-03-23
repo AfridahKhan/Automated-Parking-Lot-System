02b803e01a2955318e91cfa38d26de40153a7673
AfridahKhan <afridahkhan@gmail.com>
2025-03-23 21:42:53 +0600

    Update ParkingQueryService.java

--- src/ParkingQueryService.java
+++ src/ParkingQueryService.java
@@ -11,17 +11,29 @@ public class ParkingQueryService implements IParkingQuery{
         List<String> regNumbers = new ArrayList<>();
         for(Ticket ticket: occupiedSlots.values()){
             if(ticket.getCar().getColor().equalsIgnoreCase(color)){
-                regNumbers.add(ticket.getCar().getRegistrationNumber())
+                regNumbers.add(ticket.getCar().getRegistrationNumber());
             }
         }
         System.out.println(regNumbers.isEmpty() ? "Not found" : String.join(", ",regNumbers));
     }
     @Override
-    public void findSlotByRegistration(String color){
-
+    public void findSlotByRegistration(String regNumber){
+        for (Map.Entry<Integer, Ticket> entry : occupiedSlots.entrySet()) {
+            if (entry.getValue().getCar().getRegistrationNumber().equalsIgnoreCase(regNumber)) {
+                System.out.println(entry.getKey());
+                return;
+            }
+        }
+        System.out.println("Not found");
     }
     @Override
     public void findSlotsByColor(String color){
-
+        List<Integer> slots = new ArrayList<>();
+        for (Map.Entry<Integer, Ticket> entry : occupiedSlots.entrySet()) {
+            if (entry.getValue().getCar().getColor().equalsIgnoreCase(color)) {
+                slots.add(entry.getKey());
+            }
+        }
+        System.out.println(slots.isEmpty() ? "Not found" : slots.toString().replaceAll("[\\[\\]]", ""));
     }
 }
