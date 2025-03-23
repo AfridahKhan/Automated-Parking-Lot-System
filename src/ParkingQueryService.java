2c80d99e256763c835137cb5d58e195339c5aeaf
AfridahKhan <afridahkhan@gmail.com>
2025-03-23 21:26:56 +0600

    Update ParkingQueryService.java

--- src/ParkingQueryService.java
+++ src/ParkingQueryService.java
@@ -8,7 +8,13 @@ public class ParkingQueryService implements IParkingQuery{
 
     @Override
     public void findRegistrationByColor(String color){
-
+        List<String> regNumbers = new ArrayList<>();
+        for(Ticket ticket: occupiedSlots.values()){
+            if(ticket.getCar().getColor().equalsIgnoreCase(color)){
+                regNumbers.add(ticket.getCar().getRegistrationNumber())
+            }
+        }
+        System.out.println(regNumbers.isEmpty() ? "Not found" : String.join(", ",regNumbers));
     }
     @Override
     public void findSlotByRegistration(String color){
